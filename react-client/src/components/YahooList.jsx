fdd506d6d6864bc7a9b46459220d77ef9104dd9b
Joseph Strandmo <josephstrandmo@gmail.com>
2017-12-28 11:10:24 -0600

    refactor YahooList component as a styled component

--- react-client/src/components/YahooList.jsx
+++ react-client/src/components/YahooList.jsx
@@ -2,19 +2,39 @@ import React from 'react';
 import styled from 'styled-components';
 import YahooListItem from './YahooListItem.jsx';
 
+const Wrapper = styled.div`
+  border:2px solid black;
+  display: inline-block;
+  float: left;
+  width: 39%;
+  margin-right:1%;
+`;
+
+const Title = styled.h4`
+
+`;
+
 const YahooList = (props) => (
-  <div className="yahoo-list">
-    <h4> Yahoo Results </h4>
-    {props.results.map((result, i) => <YahooListItem className="yahoo" key={i} result={result}/>)}
-  </div>
+  <Wrapper className="yahoo-list">
+  	<Title> Yahoo Results </Title>
+  	{props.results.map((result, i) => <YahooListItem className="yahoo" key={i} result={result}/>)}
+  </Wrapper>
 )
+
+
+// const YahooList = (props) => (
+//   <div className="yahoo-list">
+//     <h4> Yahoo Results </h4>
+//     {props.results.map((result, i) => <YahooListItem className="yahoo" key={i} result={result}/>)}
+//   </div>
+// )
 
 export default YahooList;
 
 // import React from 'react';
 // import styled from 'styled-components';
 
-// // Create a Title component that'll render an <h1> tag with some styles
+// Create a Title component that'll render an <h1> tag with some styles
 // const Title = styled.h1`
 //   font-size: 1.5em;
 //   text-align: center;
